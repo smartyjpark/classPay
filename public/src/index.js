b83186c3892a86958a1bba75c9ad618339ea7f1e
박예준 <smartyjpark@gmail.com>
2017-10-12 03:21:46 +0900

    iamport pay & refund 완료 및 refund 정보 체크 로직 구현

--- public/src/index.js
+++ public/src/index.js
@@ -1,23 +1,21 @@
-import axios from 'axios'
-const config = require('../../config');
-
-
-let token;
+const axios = require('axios');
 
 function selectOption(optionClass) {
     const delegationTarget = document.querySelector(optionClass);
     delegationTarget.addEventListener('click', (e) => {
         let target = (e.target.tagName === 'SPAN') ? e.target.parentNode : e.target;
         if (!target.classList.contains("option")) return false;
-        resetOption('.selected');
+        resetOption();
         viewPlan('membership-information', target);
         target.classList.add('selected');
     })
 }
 
-function resetOption(selectedClass) {
-    document.querySelectorAll(selectedClass).forEach((item) => {
-        item.classList.remove('selected')
+function resetOption() {
+    document.querySelectorAll('.selected').forEach((item) => {
+        if (item.classList.contains('selected')) {
+            item.classList.remove('selected')
+        }
     })
 }
 
@@ -30,15 +28,64 @@ function viewPlan(infoClass, target) {
 
 function getToken() {
     axios.post('/token')
-        .then(function (response) {
-            token = response.data['access_token'];
-            console.log(token)
-        });
+}
+
+function resetValue() {
+    document.querySelectorAll('input').forEach((ele) => {
+        ele.value = "";
+    })
+}
+
+function pageChange() {
+    document.querySelector('.menu-bar').addEventListener('click', (e) => {
+        const target = e.target;
+        if (!target.classList.contains('menu-option')) return false;
+        document.querySelector('.menu-option.menu-selected').classList.remove('menu-selected')
+        target.classList.add('menu-selected');
+        document.querySelector('.content.display-on').classList.remove('display-on');
+        document.querySelector('.navigation-bar-label.nav-on').classList.remove('nav-on')
+        const content = target.dataset.page;
+        const title = target.dataset.title;
+        document.querySelector('.' + title).classList.add('nav-on');
+        document.querySelector('.' + content).classList.add('display-on');
+        menuOff();
+        layerOff();
+        resetValue();
+        if (document.querySelector('.option.selected')) document.querySelector('.option.selected').classList.remove('selected');
+    });
+}
+
+function menuOn() {
+    document.querySelector('.menu-img').addEventListener('click', () => {
+        document.querySelector('.menu-bar').classList.add('menu-on');
+        layerOn();
+    })
+}
+
+function menuOff() {
+    document.querySelector('.menu-bar').classList.remove('menu-on')
+    layerOff()
+}
+
+function layerOn() {
+    document.querySelector('.layer').classList.add('on')
+}
+
+function layerOff() {
+    document.querySelector('.layer').classList.remove('on')
+}
+
+function layerEvent() {
+    document.querySelector('.layer').addEventListener('click', () => {
+        menuOff()
+    })
 }
 
 function pay() {
     let payObj = {};
-    payObj['merchant_uid'] = "classPay_" + new Date().getTime();
+    payObj.merchant_uid = "classPay_" + new Date().getTime();
+    payObj.buyer_email = document.getElementById('pay-email').value;
+    payObj.buyer_tel = document.getElementById('pay-phone').value;
     payObj.name = document.querySelector(".option.selected").innerText;
     payObj.amount = document.querySelector(".option.selected").dataset.amount;
     payObj.buyer_name = document.getElementById('pay-name').value;
@@ -47,19 +94,64 @@ function pay() {
     payObj.birth = document.getElementById('pay-birth').value;
     payObj['pwd_2digit'] = document.getElementById('pay-pw').value;
     axios.post('/pay', payObj)
-        .then(function(res){
+        .then((res) => {
             console.log(res)
+            console.log("------")
+            console.log(res.data)
+            if (res.data.code === 0) {
+                alert('결제에 성공하였습니다. 해당 이메일로 결제정보가 전송됩니다. [주문번호]: '+ payObj.merchant_uid);
+                resetValue()
+                resetOption()
+            } else {
+                alert("결제에 실패하셧습니다. " + res.data.message);
+                resetValue()
+                resetOption()
+            }
         })
 }
 
+function refund() {
+    let refundObj = {}
+    refundObj.merchant_uid = document.getElementById('refund-uid').value;
+    refundObj.buyer_name = document.getElementById('refund-name').value;
+    refundObj.buyer_email = document.getElementById('refund-email').value;
+    axios.post('/refund', refundObj)
+        .then((res) => {
+            if (res.data.code === 0) {
+                alert("환불이 완료되었습니다. 이메일로 환불정보가 전송됩니다.");
+                resetValue();
+                resetOption();
+            } else {
+                console.log(res.data);
+                alert("환불에 실패하셧습니다. " + res.data);
+                resetValue();
+                resetOption();
+            }
+        })
+        .catch((error) => {
+            alert(error)
+        })
+
+}
+
 function payEvent() {
-    document.querySelector('.submit-btn').addEventListener('click', ()=>{
+    document.querySelector('.submit-btn').addEventListener('click', () => {
         pay()
+    })
+}
+
+function refundEvent() {
+    document.querySelector('.refund-btn').addEventListener('click', () => {
+        refund()
     })
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     getToken();
     selectOption('.option-wrapper');
-    payEvent()
+    payEvent();
+    menuOn();
+    layerEvent();
+    pageChange()
+    refundEvent()
 });
--- router/iamport.js
+++ router/iamport.js
@@ -4,11 +4,6 @@ const config = require('../config');
 
 
 let token;
-var Iamport = require('iamport');
-var iamport = new Iamport({
-    impKey: config.RESTKey,
-    impSecret: config.RESTSecretKey
-});
 
 module.exports = (app) => {
     app.post('/token', (req, res) => {
@@ -22,7 +17,6 @@ module.exports = (app) => {
         })
             .then(function(response){
                 token = response.data.response;
-                res.send(token)
             })
             .catch(function(error){
                 console.log(error);
@@ -37,12 +31,47 @@ module.exports = (app) => {
             data: req.body
         })
             .then(function(response){
-                console.log(response)
-                res.send(response.data.message)
+                res.send(response.data)
+                console.log("pay success")
             })
             .catch(function(error){
                 console.log(error);
+                res.send(response.data)
+            })
+    })
 
+    app.post('/refund', (req, res, err) => {
+        const uid = "merchant_uid";
+        const email = "buyer_email";
+        const name = "buyer_name";
+        axios({
+            method: 'get',
+            url: hostName + "/payments/find/" + req.body[uid] + "/paid",
+            headers: {"Authorization": token['access_token']},
+            data: req.body
+        })
+            .then(function(response){
+                console.log(response.data);
+                console.log(req.body[name]);
+                console.log(req.body[email]);
+                if(req.body[name] === response.data.response[name] && req.body[email] === response.data.response[email]) {
+                    axios({
+                        method: 'post',
+                        url: hostName + "/payments/cancel",
+                        headers: {"Authorization": token['access_token']},
+                        data: req.body
+                    })
+                        .then((response)=>{
+                            if(response.data)
+                            res.send(response.data)
+                        })
+                } else {
+                    res.send("구매시 입력한 정보와 다릅니다")
+                }
+            })
+            .catch(function(error){
+                console.log("error!! " + error.response.data.message);
+                res.send("구매번호를 다시 확인해주세요")
             })
     })
 
